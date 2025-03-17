9a34db9e5ad3a3f2a6aab9ba0d24c1704ba1b1e8
Vladislav Andonov <wladoandonov@gmail.com>
2025-03-17 12:06:50 +0200

    sortArray and argumentInfo refactored

--- Exercises. Advanced Functions/01-sortArray.js
+++ Exercises. Advanced Functions/01-sortArray.js
@@ -1,9 +1,23 @@
-function sortArray(arr, sortOrder) {
-  if (sortOrder == "asc") {
-    arr.sort((a, b) => a - b);
-  } else {
-    arr.sort((a, b) => b - a);
-  }
-  return arr;
+// function sortArray(arr, sortOrder) {
+//   if (sortOrder == "asc") {
+//     arr.sort((a, b) => a - b);
+//   } else {
+//     arr.sort((a, b) => b - a);
+//   }
+//   return arr;
+// }
+// sortArray([14, 7, 17, 6, 8], "asc");
+
+function solve(arr, sorting) {
+    function sortArr() {
+        return sorting == "asc"
+            ? arr.sort((a, b) => a - b)
+            : arr.sort((a, b) => b - a);
+    }
+    sortArr();
+
+    return arr;
 }
-sortArray([14, 7, 17, 6, 8], "asc");
+
+solve([14, 7, 17, 6, 8], "asc")
+solve([14, 7, 17, 6, 8], "desc");
--- Exercises. Advanced Functions/02-argumentInfo.js
+++ Exercises. Advanced Functions/02-argumentInfo.js
@@ -1,19 +1,53 @@
-function argInfo(...argArr) {
-  let result = {};
-  for (let arg of argArr) {
-    let type = typeof arg;
-    console.log(`${type}: ${arg}`);
-    if (!result.hasOwnProperty(type)) {
-      result[type] = 0;
-    }
-    result[type] += 1;
-  }
-  let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
+// function argInfo(...argArr) {
+//   let result = {};
+//   for (let arg of argArr) {
+//     let type = typeof arg;
+//     console.log(`${type}: ${arg}`);
+//     if (!result.hasOwnProperty(type)) {
+//       result[type] = 0;
+//     }
+//     result[type] += 1;
+//   }
+//   let sortedResult = Object.entries(result).sort((a, b) => b[1] - a[1]);
 
-  for (let [type, count] of sortedResult) {
-    console.log(`${type} = ${count}`);
+//   for (let [type, count] of sortedResult) {
+//     console.log(`${type} = ${count}`);
+//   }
+// }
+// argInfo("cat", 42, function () {
+//   console.log("Hello world!");
+// });
+
+function solve(...arg) {
+  let info = {};
+
+  for (el of arg) {
+    function checkType() {
+      return typeof el;
+    }
+    const type = checkType();
+
+    console.log(`${type}: ${el}`);
+
+    if (!info.hasOwnProperty(type)) {
+      info[type] = 1;
+    } else {
+      info[type] += 1;
+    }
+  }
+
+  const sortedInfo = Object.entries(info).sort((a, b) => b[1] - a[1]);
+
+  for (el of sortedInfo) {
+    console.log(`${el[0]} = ${el[1]}`);
   }
 }
-argInfo("cat", 42, function () {
-  console.log("Hello world!");
-});
+
+solve(
+  "cat",
+  42,
+  function () {
+    console.log("Hello world!");
+  },
+  43434
+);
